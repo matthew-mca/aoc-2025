2d1921f3d7ba204e461cd0360d11417a281df120
Matthew McAteer <MatthewMcA2001@gmail.com>
2025-12-03 00:35:56 +0000

    Day 2 - Part 2

--- day2/day2.exs
+++ day2/day2.exs
@@ -61,6 +61,74 @@ defmodule Day2 do
     
   end
 
+  def get_factors_of_num(num) do
+    # We only need to check up to halfway to the number,
+    # since the pattern has to repeat at LEAST twice.
+    # That lets us know the longest a substring can be
+    # is half the length of the full thing.
+    Enum.filter(1..num, fn val ->
+      if Integer.mod(num, val) == 0 and val != num do
+        true
+      else
+        false
+      end
+    end)
+  end
+
+  def is_invalid_id(product_id) do
+    str_product_id = product_id |> to_string()
+
+    product_id_len = 
+      product_id
+      |> Integer.digits()
+      |> length()
+
+    len_factors = get_factors_of_num(product_id_len)
+
+    valid_factor =
+      Enum.find(len_factors, -1, fn val ->
+        {left, _right} = String.split_at(str_product_id, val)
+        multiplier = div(product_id_len, val)
+        sequenced_id = String.duplicate(left, multiplier)
+        if sequenced_id === str_product_id do
+          true
+        else
+          false
+        end
+      end)
+
+    if valid_factor === -1 do
+      false
+    else
+      true
+    end
+  end
+
+  def get_invalid_ids_with_new_rule(id_range) do
+    start = id_range |> elem(0)
+    finish = id_range |> elem(1)
+
+    Enum.filter(start..finish, &is_invalid_id/1)
+  end
+
+  def part_2() do
+    input = read_puzzle_input()
+    # Part 2 now forces us to check different lengths of substrings.
+    # The one saving grace is the substring still has to cleanly make up
+    # the entire sequence. If we get the length of each candidate, we can
+    # brute force checking the IDs using string concatenation/multiplication.
+    invalid_id_lists = Enum.map(input, &get_invalid_ids_with_new_rule/1)
+    invalid_id_totals = 
+      Enum.map(invalid_id_lists, fn id_list ->
+        Enum.reduce(id_list, 0, fn val, total -> total + val end)
+      end)
+
+    IO.inspect(invalid_id_totals, label: "Invalid ID totals for each range")
+
+    final_total = Enum.reduce(invalid_id_totals, 0, fn val, total -> total + val end)
+    IO.inspect(final_total, label: "Final Invalid ID total")
+  end
 end
 
 Day2.part_1()
+Day2.part_2()
